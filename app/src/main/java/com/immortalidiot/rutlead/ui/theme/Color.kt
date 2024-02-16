418d6870fd2d9bd9945ec67dcb7ecb770cd3bb29
ImmortalIdiot <max.krasulya@yandex.ru>
2024-02-17 02:51:29 +0300

    feat: add the necessary colors

--- app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
+++ app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val classicWhite = Color.White
+val classicBlack = Color.Black
+val classicRed = Color.Red
+
+val primaryGrayLightTheme = Color(0xFF878787)
+val primaryGrayDarkTheme = Color(0xFFC8C8C8)
+
+val primaryDarkBlue = Color(0xFF1E4A99)
+val secondaryDarkBlue = Color(0xFF2A5EBD)
