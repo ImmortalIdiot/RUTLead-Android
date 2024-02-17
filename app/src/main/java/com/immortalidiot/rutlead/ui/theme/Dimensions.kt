b64be6b8b961645927472d989a61906cfd3abe63
ImmortalIdiot <max.krasulya@yandex.ru>
2024-02-17 03:29:45 +0300

    feat: add the necessary dimensions

---
+++ app/src/main/java/com/immortalidiot/rutlead/ui/theme/Dimensions.kt
@@ -0,0 +1,19 @@
+package com.immortalidiot.rutlead.ui.theme
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimensions(
+    val verticalSSmall: Dp = 8.dp,
+    val verticalXSmall: Dp = 10.dp,
+    val verticalNormal: Dp = 12.dp,
+    val verticalStandard: Dp = 16.dp,
+    val verticalSLarge: Dp = 20.dp,
+    val verticalXLarge: Dp = 32.dp,
+
+    val verticalSmallPadding: Dp = 4.dp,
+    val verticalNormalPadding: Dp = 6.dp,
+)
+
+val LocalDimensions = compositionLocalOf { Dimensions() }
