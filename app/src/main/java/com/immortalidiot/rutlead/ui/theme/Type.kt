563456b1fa5cbb6fade0dbcc1290f5b1f2e24da9
ImmortalIdiot <max.krasulya@yandex.ru>
2024-02-17 03:12:48 +0300

    feat: add the necessary styles

--- app/src/main/java/com/immortalidiot/rutlead/ui/theme/Type.kt
+++ app/src/main/java/com/immortalidiot/rutlead/ui/theme/Type.kt
@@ -1,12 +1,14 @@
 package com.immortalidiot.rutlead.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.immortalidiot.rutlead.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,20 +17,45 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+val InterFontFamily = FontFamily(
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold)
+)
+
+val LatoFontFamily = FontFamily(
+    Font(R.font.lato_bold, FontWeight.Bold)
+)
+
+private val mediumInterStyle = TextStyle(
+    fontFamily = InterFontFamily,
+    fontWeight = FontWeight.Medium,
+)
+
+private val semiBoldInterStyle = TextStyle(
+    fontFamily = InterFontFamily,
+    fontWeight = FontWeight.SemiBold,
+)
+
+private val boldInterStyle = TextStyle(
+    fontFamily = InterFontFamily,
+    fontWeight = FontWeight.Bold,
+)
+
+private val boldLatoStyle = TextStyle(
+    fontFamily = LatoFontFamily,
+    fontWeight = FontWeight.Bold
+)
+
+val boldInter36 = boldInterStyle.copy(fontSize = 36.sp)
+val boldInter24 = boldInterStyle.copy(fontSize = 24.sp)
+val boldInter20 = boldInterStyle.copy(fontSize = 20.sp)
+val boldInter16 = boldInterStyle.copy(fontSize = 16.sp)
+val boldInter14 = boldInterStyle.copy(fontSize = 14.sp)
+
+val boldLato18 = boldLatoStyle.copy(fontSize = 18.sp)
+val boldLato17 = boldLatoStyle.copy(fontSize = 17.sp)
+val boldLato16 = boldLatoStyle.copy(fontSize = 16.sp)
+val boldLato12 = boldLatoStyle.copy(fontSize = 12.sp)
