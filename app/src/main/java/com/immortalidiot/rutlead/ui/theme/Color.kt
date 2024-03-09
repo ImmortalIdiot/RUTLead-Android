9c5e768fcf95ceec635dcd607321a496d20be39d
Nn17 <yakor356@gmail.com>
2024-03-09 14:01:15 +0300

    refactor: remove the colors generated by the IDE

--- app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
+++ app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
@@ -2,20 +2,6 @@ package com.immortalidiot.rutlead.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple80 = Color(0xFFD0BCFF)
-val purpleGrey80 = Color(0xFFCCC2DC)
-val pink80 = Color(0xFFEFB8C8)
-
-val purple40 = Color(0xFF6650a4)
-val purpleGrey40 = Color(0xFF625b71)
-val pink40 = Color(0xFF7D5260)
-
-val classicWhite = Color.White
-val classicBlack = Color.Black
-val classicRed = Color.Red
-
-val primaryGrayLightTheme = Color(0xFF878787)
-val primaryGrayDarkTheme = Color(0xFFC8C8C8)
 val darkWhite = Color(0xFFC7D2E5)
 
 val primaryDarkBlue = Color(0xFF1E4A99)
