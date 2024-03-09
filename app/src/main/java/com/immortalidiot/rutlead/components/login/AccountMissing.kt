c2c878df57e32a5250858faa706bca3857a6f1ed
Nn17 <yakor356@gmail.com>
2024-03-09 13:58:34 +0300

    refactor: change the fist letters of color values to lower case

--- app/src/main/java/com/immortalidiot/rutlead/components/login/AccountMissing.kt
+++ app/src/main/java/com/immortalidiot/rutlead/components/login/AccountMissing.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.immortalidiot.rutlead.R
-import com.immortalidiot.rutlead.ui.theme.DarkWhite
+import com.immortalidiot.rutlead.ui.theme.darkWhite
 import com.immortalidiot.rutlead.ui.theme.boldInter14
 
 @Composable
@@ -14,7 +14,7 @@ fun AccountMissing() {
     Text(
         text = stringResource(id = R.string.account_missing),
         style = boldInter14,
-        color = if (isSystemInDarkTheme()) DarkWhite
+        color = if (isSystemInDarkTheme()) darkWhite
         else Color.White
     )
 }
--- app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
+++ app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
@@ -2,13 +2,13 @@ package com.immortalidiot.rutlead.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val purple80 = Color(0xFFD0BCFF)
+val purpleGrey80 = Color(0xFFCCC2DC)
+val pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val purple40 = Color(0xFF6650a4)
+val purpleGrey40 = Color(0xFF625b71)
+val pink40 = Color(0xFF7D5260)
 
 val classicWhite = Color.White
 val classicBlack = Color.Black
@@ -16,14 +16,14 @@ val classicRed = Color.Red
 
 val primaryGrayLightTheme = Color(0xFF878787)
 val primaryGrayDarkTheme = Color(0xFFC8C8C8)
-val DarkWhite = Color(0xFFC7D2E5)
+val darkWhite = Color(0xFFC7D2E5)
 
 val primaryDarkBlue = Color(0xFF1E4A99)
-val DarkBlue = Color(0xFF2552A3)
-val LightBlue = Color(0xFF2A5EBD)
+val darkBlue = Color(0xFF2552A3)
+val lightBlue = Color(0xFF2A5EBD)
 
-val DarkBlack = Color(0xFF1C1B1B)
-val LightBlack = Color(0xFF232323)
+val darkBlack = Color(0xFF1C1B1B)
+val lightBlack = Color(0xFF232323)
 
 
 sealed class ThemeColors(
@@ -39,25 +39,25 @@ sealed class ThemeColors(
 ) {
     object Light : ThemeColors(
         surface = Color.White,
-        primary = LightBlue,
+        primary = lightBlue,
         outline = Color.Transparent,
-        text = LightBlue,
+        text = lightBlue,
         container = Color.White,
         label = Color.White,
         buttonOutline = Color.White,
-        textSelection = DarkWhite,
-        handle = DarkBlue
+        textSelection = darkWhite,
+        handle = darkBlue
     )
 
     object Dark : ThemeColors(
-        surface = DarkBlack,
-        primary = LightBlack,
+        surface = darkBlack,
+        primary = lightBlack,
         outline = primaryDarkBlue,
-        text = DarkWhite,
-        container = LightBlack,
-        label = DarkWhite,
-        buttonOutline = DarkBlue,
+        text = darkWhite,
+        container = lightBlack,
+        label = darkWhite,
+        buttonOutline = darkBlue,
         textSelection = primaryDarkBlue,
-        handle = DarkWhite
+        handle = darkWhite
     )
 }
