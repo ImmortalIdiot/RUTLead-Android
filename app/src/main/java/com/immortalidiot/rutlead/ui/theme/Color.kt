b7a5a1a9353182bd5da566715ef47fd95cb213ff
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-13 22:23:00 +0300

    refactor: change the color palette; add new colors

--- app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
+++ app/src/main/java/com/immortalidiot/rutlead/ui/theme/Color.kt
@@ -2,48 +2,71 @@ package com.immortalidiot.rutlead.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val darkWhite = Color(0xFFC7D2E5)
+val DarkWhite = Color(0xFFC7D2E5)
+val ClassicWhite = Color.White
 
-val primaryDarkBlue = Color(0xFF1E4A99)
-val darkBlue = Color(0xFF2552A3)
-val lightBlue = Color(0xFF2A5EBD)
+val PrimaryDarkBlue = Color(0xFF1E4A99)
+val DarkBlue = Color(0xFF2552A3)
+val LightBlue = Color(0xFF2A5EBD)
 
-val darkBlack = Color(0xFF1C1B1B)
-val lightBlack = Color(0xFF232323)
+val ClassicGray = Color.Gray
+val DarkGray = Color.DarkGray
+val LightGray = Color.LightGray
+
+val DarkBlack = Color(0xFF1C1B1B)
+val LightBlack = Color(0xFF232323)
 
 
 sealed class ThemeColors(
+    val header: Color,
     val surface: Color,
     val primary: Color,
+    val content: Color,
     val outline: Color,
     val text: Color,
+    val containerText: Color,
     val container: Color,
     val label: Color,
+    val labelText: Color,
+    val cursor: Color,
     val buttonOutline: Color,
     val textSelection: Color,
-    val handle: Color
+    val handle: Color,
+    val handleBackground: Color
 ) {
     object Light : ThemeColors(
-        surface = Color.White,
-        primary = lightBlue,
+        header = ClassicWhite,
+        surface = Color.Transparent,
+        primary = LightBlue,
+        content = LightBlue,
         outline = Color.Transparent,
-        text = lightBlue,
-        container = Color.White,
-        label = Color.White,
+        text = ClassicWhite,
+        container = ClassicWhite,
+        containerText = LightBlue,
+        label = ClassicWhite,
+        labelText = LightBlue,
+        cursor = LightBlue,
         buttonOutline = Color.White,
-        textSelection = darkWhite,
-        handle = darkBlue
+        textSelection = DarkWhite,
+        handle = LightGray,
+        handleBackground = LightGray
     )
 
     object Dark : ThemeColors(
-        surface = darkBlack,
-        primary = lightBlack,
-        outline = primaryDarkBlue,
-        text = darkWhite,
-        container = lightBlack,
-        label = darkWhite,
-        buttonOutline = darkBlue,
-        textSelection = primaryDarkBlue,
-        handle = darkWhite
+        header = DarkWhite,
+        surface = DarkBlack,
+        primary = LightBlack,
+        content = DarkWhite,
+        outline = PrimaryDarkBlue,
+        text = DarkWhite,
+        container = LightBlack,
+        containerText = DarkWhite,
+        label = DarkWhite,
+        labelText = DarkWhite,
+        cursor = DarkBlue,
+        buttonOutline = DarkBlue,
+        textSelection = PrimaryDarkBlue,
+        handle = DarkWhite,
+        handleBackground = LightBlue
     )
 }
