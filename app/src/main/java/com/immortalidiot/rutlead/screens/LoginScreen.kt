3b4a9eb3bd22938744d86c8b6c95dea9c9d3663b
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-14 00:02:23 +0300

    refactor: completely refactor of the login screen

--- app/src/main/java/com/immortalidiot/rutlead/screens/LoginScreen.kt
+++ app/src/main/java/com/immortalidiot/rutlead/screens/LoginScreen.kt
@@ -14,13 +14,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.immortalidiot.rutlead.R
 import com.immortalidiot.rutlead.buttons.login.SignInButton
@@ -30,67 +29,105 @@ import com.immortalidiot.rutlead.components.login.RedirectText
 import com.immortalidiot.rutlead.fields.PasswordField
 import com.immortalidiot.rutlead.fields.StudentIdTextField
 import com.immortalidiot.rutlead.ui.theme.LocalDimensions
+import com.immortalidiot.rutlead.ui.theme.ThemeColors
 
 @Preview(showBackground = true)
 @Composable
-fun LoginDesign() {
+fun LoginDesign(
+    modifier: Modifier = Modifier,
+    isDarkTheme: Boolean = isSystemInDarkTheme()
+) {
     val dimensions = LocalDimensions.current
+    val roundedShape = RoundedCornerShape(dimensions.shapeXLarge)
+    val palette = if (isDarkTheme) ThemeColors.Dark
+                      else ThemeColors.Light
 
-    Surface(
-        modifier = Modifier
+    Column(
+        modifier = modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.surface),
+            .background(color = palette.surface),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Image(
-                painter = painterResource(
-                    id = if (isSystemInDarkTheme()) R.drawable.ic_app_dark_logo
-                         else R.drawable.ic_app_light_logo
-                ),
-                contentDescription = "logo",
-                modifier = Modifier
-                    .fillMaxWidth(0.4f)
-                    .fillMaxHeight(0.07f)
-            )
-            Spacer(modifier = Modifier.height(dimensions.verticalXXLarge))
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxWidth(0.85f)
-                    .fillMaxHeight(0.65f)
-                    .clip(RoundedCornerShape(dimensions.shapeXLarge))
-                    .background(MaterialTheme.colorScheme.primary)
-                    .border(
-                        width = dimensions.borderSSmall,
-                        color = MaterialTheme.colorScheme.outline,
-                        shape = RoundedCornerShape(dimensions.shapeXLarge)
-                    )
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth(0.85f)
-                ) {
-                    BoxLabel()
-                    Spacer(modifier = Modifier.height(dimensions.verticalXXLarge))
-                    StudentIdTextField("Номер студенческого билета")
-                    Spacer(modifier = Modifier.height(dimensions.verticalXLarge))
-                    PasswordField("Пароль")
-                    Spacer(modifier = Modifier.height(dimensions.verticalXLarge))
-                    SignInButton()
-                    Spacer(modifier = Modifier.height(dimensions.verticalXLarge))
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        AccountMissing()
-                        RedirectText(text = "Зарегистрируйтесь")
-                    }
-                    Spacer(modifier = Modifier.height(dimensions.verticalXSmall))
-                    RedirectText(text = "Забыли пароль?")
+        Image(
+            modifier = modifier
+                .fillMaxWidth(0.4f)
+                .fillMaxHeight(0.07f),
+            imageVector = ImageVector.vectorResource(
+                id = if (isSystemInDarkTheme()) {
+                    R.drawable.ic_app_dark_logo
+                } else {
+                    R.drawable.ic_app_light_logo
                 }
+            ),
+            contentDescription = "logo",
+        )
+        Spacer(modifier = modifier.height(dimensions.verticalXXLarge))
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier
+                .fillMaxWidth(0.85f)
+                .fillMaxHeight(0.65f)
+                .clip(roundedShape)
+                .background(color = palette.primary)
+                .border(
+                    width = dimensions.borderSSmall,
+                    color = palette.outline,
+                    shape = roundedShape
+                )
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier
+                    .fillMaxWidth(0.85f)
+            ) {
+                BoxLabel(
+                    text = "Авторизация",
+                    palette = palette
+                )
+                Spacer(modifier = modifier.height(dimensions.verticalXXLarge))
+                StudentIdTextField(
+                    hint = "Номер студенческого билета",
+                    palette = palette
+                )
+                Spacer(modifier = modifier.height(dimensions.verticalXLarge))
+                PasswordField(
+                    hint = "Пароль",
+                    palette = palette,
+                    modifier = modifier
+                        .border(
+                            width = dimensions.borderSSmall,
+                            color = palette.outline,
+                            shape = roundedShape
+                        )
+                )
+                Spacer(modifier = Modifier.height(dimensions.verticalXLarge))
+                SignInButton(
+                    modifier = modifier,
+                    palette = palette,
+                    text = "Войти",
+                    onButtonClick = {
+                        // TODO: realise body of the fun when the view model appears
+                    },
+                )
+                Spacer(modifier = modifier.height(dimensions.verticalXLarge))
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    AccountMissing(
+                        palette = palette
+                    )
+                    RedirectText(
+                        text = "Зарегистрируйтесь",
+                        palette = palette
+                    )
+                }
+                Spacer(modifier = modifier.height(dimensions.verticalXSmall))
+                RedirectText(
+                    text = "Забыли пароль?",
+                    palette = palette
+                )
             }
         }
     }
