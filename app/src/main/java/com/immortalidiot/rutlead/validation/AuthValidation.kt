b272e4fade92aa8d3f23fe1806cc5ef6a52af630
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-14 14:20:43 +0300

    feat: add user input validation at the login stage

---
+++ app/src/main/java/com/immortalidiot/rutlead/validation/AuthValidation.kt
@@ -0,0 +1,49 @@
+package com.immortalidiot.rutlead.validation
+
+class AuthValidationException(message: String) : Exception(message = message)
+
+fun String.validateStudentID(): Result<Boolean> {
+    val containsOnlyDigits = this.all { char ->
+        char.isDigit()
+    }
+
+    return if (this.isNotBlank() && containsOnlyDigits && this.length == 8) {
+        Result.success(true)
+    } else if (this.isBlank()) {
+        Result.failure(
+            AuthValidationException(
+                "Поле \"Номер студенческого билета\" не должно быть пустым"
+            )
+        )
+    } else if (!containsOnlyDigits) {
+        Result.failure(
+            AuthValidationException(
+                "Поле \"Номер студенческого билета\" должно содержать только цифры"
+            )
+        )
+    } else {
+        Result.failure(
+            AuthValidationException(
+                "Номер студенческого билета состоит из 8 цифр"
+            )
+        )
+    }
+}
+
+fun String.validatePassword(): Result<Boolean> {
+    return if (this.isNotBlank() && this.length >= 8) {
+        Result.success(true)
+    } else if (isBlank()) {
+        Result.failure(
+            AuthValidationException(
+                "Поле \"Пароль\" не должно быть пустым"
+            )
+        )
+    } else {
+        Result.failure(
+            AuthValidationException(
+                "Пароль должен содержать минимум 8 символов"
+            )
+        )
+    }
+}
