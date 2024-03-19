566cba3bb6c94b3ef05d35354434729f9440d65b
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-19 04:42:50 +0300

    feat: add signup model

---
+++ app/src/main/java/com/immortalidiot/rutlead/models/SignUpModel.kt
@@ -0,0 +1,9 @@
+package com.immortalidiot.rutlead.models
+
+data class SignUpModel(
+    val email: String,
+    val studentID: String,
+    val password: String,
+    val group: String,
+    val name: String
+)
