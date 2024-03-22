dc5792f4e8e3e11b098efb423acb49c1b708f334
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-23 01:35:08 +0300

    refactor: add isFocused and isPasswordVisible params to the model

--- app/src/main/java/com/immortalidiot/rutlead/models/ResetPasswordModel.kt
+++ app/src/main/java/com/immortalidiot/rutlead/models/ResetPasswordModel.kt
@@ -3,5 +3,7 @@ package com.immortalidiot.rutlead.models
 data class ResetPasswordModel(
     val email: String,
     val password: String,
-    val confirmPassword: String
+    val confirmPassword: String,
+    val isFocused: Boolean,
+    val isPasswordVisible: Boolean
 )
