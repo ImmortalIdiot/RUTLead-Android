3a44bb9ee0f653307278f12bd876b337b42f15fb
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-26 01:58:16 +0300

    feat: connect the basic screen flow to the main activity

--- app/src/main/java/com/immortalidiot/rutlead/MainActivity.kt
+++ app/src/main/java/com/immortalidiot/rutlead/MainActivity.kt
@@ -3,33 +3,43 @@ package com.immortalidiot.rutlead
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.navigation.compose.rememberNavController
+import com.immortalidiot.rutlead.navigation.main.RUTLeadScreenFlow
 import com.immortalidiot.rutlead.providers.LocalSnackbarHostState
-import com.immortalidiot.rutlead.presentation.screens.auth.LoginScreen
 import com.immortalidiot.rutlead.ui.theme.RUTLeadTheme
-import com.immortalidiot.rutlead.ui.theme.ThemeColors
-import com.immortalidiot.rutlead.presentation.viemodels.auth.LoginScreenViewModel
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalComposeUiApi::class)
+    @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RUTLeadTheme {
                 val snackbarHostState = remember { SnackbarHostState() }
-
-                val keyboardController = LocalSoftwareKeyboardController.current
+                val navController = rememberNavController()
+                var isNavigationBarVisible by remember { mutableStateOf(false) }
 
                 CompositionLocalProvider(LocalSnackbarHostState provides snackbarHostState) {
-
+                    Scaffold(
+                        bottomBar = {
+                            // TODO(): create custom bottom bar
+                        }
+                    ) { padding ->
+                        RUTLeadScreenFlow(
+                            paddingValues = padding,
+                            isNavigationBarVisible = { isNavigationBarVisible = it },
+                            navController = navController
+                        )
+                    }
                 }
             }
         }
