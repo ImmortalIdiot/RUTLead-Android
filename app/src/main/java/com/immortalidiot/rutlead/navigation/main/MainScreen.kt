b23e348bb470ea0777481bc933076b30460fb4f8
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-26 13:32:19 +0300

    feat: add the main user screen

---
+++ app/src/main/java/com/immortalidiot/rutlead/navigation/main/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.immortalidiot.rutlead.navigation.main
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+sealed class MainScreen (val route: String) {
+    object JournalScreen: MainScreen("Журнал")
+    object ProfileScreen: MainScreen("Профиль")
+    object SettingsScreen: MainScreen("Настройки")
+}
