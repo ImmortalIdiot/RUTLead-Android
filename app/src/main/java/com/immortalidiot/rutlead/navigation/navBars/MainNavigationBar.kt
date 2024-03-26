96b09b4c9fae81365ec300bb869bae6140608506
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-26 23:59:07 +0300

    refactor: redesign nav bar item

--- app/src/main/java/com/immortalidiot/rutlead/navigation/navBars/MainNavigationBar.kt
+++ app/src/main/java/com/immortalidiot/rutlead/navigation/navBars/MainNavigationBar.kt
@@ -2,13 +2,16 @@ package com.immortalidiot.rutlead.navigation.navBars
 
 import androidx.compose.foundation.background
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalAbsoluteTonalElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,14 +30,17 @@ fun BottomNavigationBar(
     val currentRoute = navigationBackStackEntry?.destination?.route
 
     NavigationBar(
-        modifier = modifier.background(Color.Transparent)
+        modifier = modifier.background(
+            color = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                LocalAbsoluteTonalElevation.current)
+        )
     ) {
         navigationBarItems.forEach { item: NavigationBarItem ->
             val isSelected = currentRoute == item.route
             NavigationBarItem(
                 selected = (currentRoute == item.route),
                 onClick = {
-                      if (currentRoute != item.route) { navController.navigate(item.route) }
+                    if (currentRoute != item.route) { navController.navigate(item.route) }
                 },
                 icon = {
                     Icon(
@@ -43,7 +49,12 @@ fun BottomNavigationBar(
                         tint = if (isSelected) LightBlue else ClassicGray
                     )
                 },
-                label = { Text(text = item.name) }
+                label = { Text(text = item.name) },
+                alwaysShowLabel = false,
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                        LocalAbsoluteTonalElevation.current)
+                )
             )
         }
     }
